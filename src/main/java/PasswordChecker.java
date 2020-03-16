ffc21daf4634ed34ef12f80f2fade2c48cc26a0b
mongimpupumiso <mongimpupumiso97@gmail.com>
2020-03-16 13:25:01 +0200

    password checker

--- src/main/java/PasswordChecker.java
+++ src/main/java/PasswordChecker.java
@@ -1,139 +1,101 @@
+// PASSWORD CHECKER.
 
-class PasswordChecker extends Exception {
-    int passwordInvalid = 0;
-
-    public PasswordChecker(int conditionViolated) {
-        super("Invalid Password: ");
-        passwordInvalid = conditionViolated;
-    }
-
-    public String printMessage() {
-        switch (passwordInvalid) {
-
-            // password should exist
-            case 1:
-                return ("password should exist");
-
-            // Password should be at least 8 characters.
-            case 2:
-                return ("password should be longer than than 8 characters.");
-
-            // Password should have at least one lowercase letter
-            case 3:
-                return ("password should have at least one lowercase letter.");
-
-            // Password should have at least one uppercase letter.
-            case 4:
-                return ("password should have at least one uppercase letter.");
-
-            // Password should at least have one digit.
-            case 5:
-                return ("password should at least have one digit.");
-
-            // Password should have at least one special character.
-            case 6:
-                return ("password should have at least one special character.");
-
-        }
-        return ("");
-    }
-
+class PasswordChecker{
 
 // The function to check the validity of the password.
 
-    public static void passwordIsValid(String password) throws PasswordChecker {
+    public static boolean passwordIsValid(String password) {
+        int count = 0;
+        boolean isSpecialChar = false;
+        boolean isUpperCase = false;
+        boolean isLowerCase = false;
 
-// Check password does not exceed 8 characters.
-        if (!((password.length() >= 7) && (password.length() <= 8))) {
-            throw new PasswordChecker(1);
-        }
-
-        if (true) {
-            int count = 0;
-
-// Check 0 to 9 digits.
-            for (int i = 0; i <= 9; i++) {
-
-// Converting integer to string.
-                String str1 = Integer.toString(i);
-                if (password.contains(str1)) {
-                    count = 1;
-                }
-            }
-            if (count == 0) {
-                throw new PasswordChecker(2);
-            }
-        }
 
 // Check special characters.
-        if (!(password.contains("@") || password.contains("#") || password.contains("!")
-                || password.contains("~") || password.contains("$") || password.contains("%")
-                ||password.contains("*")|| password.contains("^") || password.contains("&")))
+        for (int m = 0; m < password.length(); m++) {
+            if (!Character.isLetterOrDigit(password.charAt(m))) {
+                isSpecialChar = true;
+                break;
+            }
+        }
+        if (isSpecialChar) {
+            count++;
+        } else
+            System.out.println("Password should have special");
+
+
+// Check for uppercase.
+
+
+        for (int m = 0; m < password.length(); m++) {
+            if (Character.isUpperCase(password.charAt(m))) {
+                isUpperCase = true;
+                break;
+            }
+            }
+            if (isUpperCase) {
+                count++;
+            } else
+                System.out.println("Password should have uppercase");
+
+
+// Check for lowercase.
+
+
+        for (int m = 0; m < password.length(); m++) {
+            if (Character.isLowerCase(password.charAt(m))) {
+                isLowerCase = false;
+                break;
+            }
+            }
+            if (isLowerCase) {
+                count++;
+            } else
+                System.out.println("Password should have lowercase");
+
+
+
+// Check for digits.
+
+        boolean isDigit = false;
+        for (int m = 0; m < password.length(); m++) {
+            if (Character.isDigit(password.charAt(m))) {
+                isDigit = false;
+                break;
+            }
+            }
+            if (isDigit) {
+                count++;
+            } else
+                System.out.println("Password should have a digit");
+
+
+// Check the length of password.
+
+            if (password.length() <= 8) {
+                count++;
+
+            }
+            else {
+                System.out.println("Password should not be more than 8 characters");
+
+        }
+
+        if(count >= 3)
         {
-            throw new PasswordChecker(3);
+            return true;
+
         }
+            return false;
+    }
 
-        if (true) {
-            int count = 0;
 
-// Check for capital letters.
-            for (int i = 65; i <= 90; i++) {
+   // THE MAIN METHOD.
 
-// Data type casting.
-                char c = (char) i;
+        public static void main (String[]args){
 
-                String str1 = Character.toString(c);
-                if (password.contains(str1)) {
-                    count = 1;
-                }
-            }
-            if (count == 0) {
-                throw new PasswordChecker(4);
-            }
-        }
-
-        if (true) {
-            int count = 0;
-
-// Check for small letters.
-
-            for (int i = 90; i <= 122; i++) {
-
-// Data type casting.
-                char c = (char) i;
-                String str1 = Character.toString(c);
-
-                if (password.contains(str1)) {
-                    count = 1;
-                }
-            }
-            if (count == 0) {
-                throw new PasswordChecker(5);
-            }
+        System.out.println( passwordIsValid(""));
         }
 
     }
 
-    // THE MAIN METHOD.
-    public static void main(String[] args) {
-        String password_1 = "Mongi@99";
-        try {
-            System.out.println("Is your Password " + password_1 + " valid?");
-            passwordIsValid(password_1);
-            System.out.println("Thank your password is valid");
-        } catch (PasswordChecker e) {
-            System.out.print(e.getMessage());
-            System.out.println(e.printMessage());
-        }
-
-        String password2 = "Mongi@99$$Mpupumiso";
-        try {
-            System.out.println("Is Password "+ password2 + " valid?");
-            passwordIsValid(password2);
-            System.out.println("Valid Password");
-        } catch (PasswordChecker e) {
-            System.out.print(e.getMessage());
-            System.out.println(e.printMessage());
-        }
-    }
-}
